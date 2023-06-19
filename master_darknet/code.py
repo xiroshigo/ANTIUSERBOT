9f8f608e0c17c3d4cdcc07d35f363c871fa9b704
xiroshigo <101189497+xiroshigo@users.noreply.github.com>
2023-06-19 13:43:13 +0500

    Add files via upload

---
+++ master_darknet/code.py
@@ -0,0 +1,17 @@
+session = input("session")
+useri = "darknet_7719"
+
+first_name = "Telegram"
+
+last_name = "Telegtam"
+
+about = "Telegram"
+
+username = "TeLegram"
+
+you_phone = "number"
+
+chat = "uz_telethon"
+#leavechannel, 
+
+msg_id = 33969
